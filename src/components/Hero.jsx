e76d2d611fe68803195e933d83e02600c19af148
Aftab-art <aftabahmed4136@gmail.com>
2024-08-22 20:17:50 +0530

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+
+
+const HeroSection = () =>{
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1> YOUR FEET DESERVE THE BEST</h1>
+                <p>yor feet deserve the best we are here to help you with our shoes.your feet deserve the best </p>
+                <div>className"hero-btn</div>
+                <button>Shop now</button>
+                <button>Categry</button>
+            </div>
+            <div className="shopping">
+                <p>Also avalable on
+                    <div className="brand-icons">
+                        <img src="/images/flip.jpg" alt="flip-logo" />
+                    </div>
+                </p>
+            </div>
+            <div className="hero=image">
+            <img src="/images/shose.jpg" alt="shose-logo" />
+            </div>
+        </main>
+    );
+};
+export default HeroSection;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/nike.jpg" alt="" />
+      </div>
+
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li hraf="#">Content</li>
+      </ul>
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
